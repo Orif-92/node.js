8aa5bafdc12cdc180c5a9663a514c58ee7bf83a7
Orif-92 <sarniyozovorif@gmail.com>
2023-09-12 08:11:58 +0500

    Get :id

--- app.js
+++ app.js
@@ -5,15 +5,36 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'application/json');
 
   if (req.method === 'GET' && req.url === '/books') {
+    // books.json faylini o'qish
     fs.readFile('books.json', 'utf8', (err, data) => {
       if (err) {
         res.statusCode = 500;
-        res.end(JSON.stringify({ error: 'Server xatosi' }));
-        return;
+        res.end(JSON.stringify({ message: 'Serverda xatolik yuz berdi' }));
+      } else {
+        res.statusCode = 200;
+        res.end(data);
       }
-      const books = JSON.parse(data);
-      res.statusCode = 200;
-      res.end(JSON.stringify(books));
+    });
+  } else if (req.method === 'GET' && req.url.startsWith('/books/')) {
+    // :id bo'yicha qidirish
+    const id = req.url.split('/').pop(); // URL dan id ni olish
+
+    fs.readFile('books.json', 'utf8', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end(JSON.stringify({ message: 'Serverda xatolik yuz berdi' }));
+      } else {
+        const books = JSON.parse(data);
+        const book = books.find((item) => item.id === parseInt(id));
+
+        if (book) {
+          res.statusCode = 200;
+          res.end(JSON.stringify(book));
+        } else {
+          res.statusCode = 404;
+          res.end(JSON.stringify({ message: 'Ma\'lumot topilmadi' }));
+        }
+      }
     });
   } else {
     res.statusCode = 404;
