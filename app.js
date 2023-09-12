87c218d266052029ddf647bf139c24a45ad18b66
Orif-92 <sarniyozovorif@gmail.com>
2023-09-12 13:36:58 +0500

    put, delete

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  
+
   if (req.method === 'GET' && req.url === '/books') {
-    fs.readFile('books.json', (err, data) => {
+    // GET /books => Barcha kitoblarni o'qish
+    fs.readFile('books.json', 'utf8', (err, data) => {
       if (err) {
         res.statusCode = 500;
         res.end(JSON.stringify({ message: 'Xatolik yuz berdi' }));
@@ -15,14 +16,16 @@ const server = http.createServer((req, res) => {
       }
     });
   } else if (req.method === 'GET' && req.url.startsWith('/books/')) {
-    const id = req.url.split('/').pop();
-    fs.readFile('books.json', (err, data) => {
+    // GET /books/:id => Kitobni ID bo'yicha izlash
+    const id = req.url.split('/')[2]; // :id ni olish
+    fs.readFile('books.json', 'utf8', (err, data) => {
       if (err) {
         res.statusCode = 500;
         res.end(JSON.stringify({ message: 'Xatolik yuz berdi' }));
       } else {
         const books = JSON.parse(data);
-        const book = books.find((b) => b.id === parseInt(id));
+        const book = books.find((b) => b.id == id);
+
         if (book) {
           res.statusCode = 200;
           res.end(JSON.stringify(book));
@@ -33,35 +36,113 @@ const server = http.createServer((req, res) => {
       }
     });
   } else if (req.method === 'POST' && req.url === '/books') {
-    let body = '';
+    // POST /books => Yangi kitob qo'shish
+    let requestBody = '';
     req.on('data', (chunk) => {
-      body += chunk.toString();
+      requestBody += chunk;
     });
+
     req.on('end', () => {
       try {
-        const newBook = JSON.parse(body);
-        fs.readFile('books.json', (err, data) => {
+        const newBook = JSON.parse(requestBody);
+        fs.readFile('books.json', 'utf8', (err, data) => {
           if (err) {
             res.statusCode = 500;
             res.end(JSON.stringify({ message: 'Xatolik yuz berdi' }));
           } else {
             const books = JSON.parse(data);
-            newBook.id = books.length + 1;
-            books.push(newBook);
-            fs.writeFile('books.json', JSON.stringify(books), (err) => {
-              if (err) {
-                res.statusCode = 500;
-                res.end(JSON.stringify({ message: 'Xatolik yuz berdi' }));
-              } else {
-                res.statusCode = 201;
-                res.end(JSON.stringify(newBook));
-              }
-            });
+            const existingBook = books.find((b) => b.title === newBook.title);
+
+            if (existingBook) {
+              res.statusCode = 409;
+              res.end(JSON.stringify({ message: 'Bu kitob bazada mavjud' }));
+            } else {
+              newBook.id = books.length + 1;
+              books.push(newBook);
+              fs.writeFile('books.json', JSON.stringify(books, null, 2), (err) => {
+                if (err) {
+                  res.statusCode = 500;
+                  res.end(JSON.stringify({ message: 'Xatolik yuz berdi' }));
+                } else {
+                  res.statusCode = 201;
+                  res.end(JSON.stringify(newBook));
+                }
+              });
+            }
           }
         });
       } catch (error) {
         res.statusCode = 400;
-        res.end(JSON.stringify({ message: 'Noto\'g\'ri so\'rovnoma formati' }));
+        res.end(JSON.stringify({ message: 'Noto‘g‘ri so‘rov yuborildi' }));
+      }
+    });
+  } else if (req.method === 'PUT' && req.url.startsWith('/books/')) {
+    // PUT /books/:id => Kitobni ID bo'yicha o'zgartirish
+    const id = req.url.split('/')[2]; // :id ni olish
+    let requestBody = '';
+    req.on('data', (chunk) => {
+      requestBody += chunk;
+    });
+
+    req.on('end', () => {
+      try {
+        const updatedBook = JSON.parse(requestBody);
+        fs.readFile('books.json', 'utf8', (err, data) => {
+          if (err) {
+            res.statusCode = 500;
+            res.end(JSON.stringify({ message: 'Xatolik yuz berdi' }));
+          } else {
+            const books = JSON.parse(data);
+            const index = books.findIndex((b) => b.id == id);
+
+            if (index !== -1) {
+              books[index] = { ...books[index], ...updatedBook };
+              fs.writeFile('books.json', JSON.stringify(books, null, 2), (err) => {
+                if (err) {
+                  res.statusCode = 500;
+                  res.end(JSON.stringify({ message: 'Xatolik yuz berdi' }));
+                } else {
+                  res.statusCode = 200;
+                  res.end(JSON.stringify(books[index]));
+                }
+              });
+            } else {
+              res.statusCode = 404;
+              res.end(JSON.stringify({ message: 'Ma\'lumot topilmadi' }));
+            }
+          }
+        });
+      } catch (error) {
+        res.statusCode = 400;
+        res.end(JSON.stringify({ message: 'Noto‘g‘ri so‘rov yuborildi' }));
+      }
+    });
+  } else if (req.method === 'DELETE' && req.url.startsWith('/books/')) {
+    // DELETE /books/:id => Kitobni ID bo'yicha o'chirish
+    const id = req.url.split('/')[2]; // :id ni olish
+    fs.readFile('books.json', 'utf8', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end(JSON.stringify({ message: 'Xatolik yuz berdi' }));
+      } else {
+        const books = JSON.parse(data);
+        const index = books.findIndex((b) => b.id == id);
+
+        if (index !== -1) {
+          const deletedBook = books.splice(index, 1)[0];
+          fs.writeFile('books.json', JSON.stringify(books, null, 2), (err) => {
+            if (err) {
+              res.statusCode = 500;
+              res.end(JSON.stringify({ message: 'Xatolik yuz berdi' }));
+            } else {
+              res.statusCode = 200;
+              res.end(JSON.stringify(deletedBook));
+            }
+          });
+        } else {
+          res.statusCode = 404;
+          res.end(JSON.stringify({ message: 'Ma\'lumot topilmadi' }));
+        }
       }
     });
   } else {
